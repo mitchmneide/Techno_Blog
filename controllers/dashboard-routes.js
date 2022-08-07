3cb1ee6ab3619f76aa7bbe8cedcfd47bf721a48b
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-08-07 18:49:00 -0500

    dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,5 +3,67 @@ const sequelize = require('../config/connection');
 const {Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    
-})
+    Post.findAll({
+        where : {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'post_body', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        const posts= dbPostData.map(post => post.get({plain: true}));
+        res.render('dashboard', {
+            posts,
+            loggedIn: true
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', (req, res) => {
+    Post.findOne (req.params.id, {
+        attributes: ['id', 'post_body', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        if (dbPostData) {
+            const post = dbPostData.map(post => post.get({plain: true}));
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            });
+        }else {
+            res. status(404).end();
+        }
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router; 
